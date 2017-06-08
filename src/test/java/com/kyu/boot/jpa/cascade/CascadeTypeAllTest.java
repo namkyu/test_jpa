fa942ce40d6a299b3a7c11678a2608fde241fccb
이남규 <nklee@ncsoft.com>
2017-06-08 14:58:57 +0900

    orphanRemoval 사용 방법에 대해서 이해

--- src/test/java/com/kyu/boot/jpa/cascade/CascadeTypeAllTest.java
+++ src/test/java/com/kyu/boot/jpa/cascade/CascadeTypeAllTest.java
@@ -15,8 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 /**
  * @Project : test_project
@@ -80,6 +79,17 @@ public class CascadeTypeAllTest {
         accountPhone1.setNumber("010-2222-2222");
         account.addPhone(accountPhone1);
 
+        // Address
+        AccountAddress accountAddress = new AccountAddress();
+        accountAddress.setId(1);
+        accountAddress.setAddress("경기도");
+        account.addAddress(accountAddress);
+
+        AccountAddress accountAddress1 = new AccountAddress();
+        accountAddress1.setId(2);
+        accountAddress1.setAddress("서울");
+        account.addAddress(accountAddress1);
+
         System.out.println("Start Test Data ---------------------------------------");
         em.persist(account); // save
         em.flush(); // DB 반영
@@ -92,15 +102,15 @@ public class CascadeTypeAllTest {
      */
     @Test
     @Transactional
-    public void Member엔티티저장상태() {
+    public void Member_엔티티저장상태() {
         memberTestData();
 
-        HomeAddress entityHomeAddress = em.find(HomeAddress.class, 1);
-        assertThat("경기도", is(entityHomeAddress.getAddress()));
-        assertThat("남규", is(entityHomeAddress.getMember().getName()));
+        HomeAddress homeAddress = em.find(HomeAddress.class, 1);
+        assertThat("경기도", is(homeAddress.getAddress()));
+        assertThat("남규", is(homeAddress.getMember().getName()));
 
-        Member entityMember = em.find(Member.class, 1);
-        assertThat("남규", is(entityMember.getName()));
+        Member member = em.find(Member.class, 1);
+        assertThat("남규", is(member.getName()));
     }
 
     /**
@@ -108,28 +118,82 @@ public class CascadeTypeAllTest {
      */
     @Test
     @Transactional
-    public void Member엔티티삭제() {
+    public void Member_엔티티삭제() {
         memberTestData();
 
-        HomeAddress entityHomeAddress = em.find(HomeAddress.class, 1);
-        em.remove(entityHomeAddress);
+        HomeAddress homeAddress = em.find(HomeAddress.class, 1);
+        em.remove(homeAddress);
         em.flush();
 
-        entityHomeAddress = em.find(HomeAddress.class, 1);
-        assertNull(entityHomeAddress);
+        homeAddress = em.find(HomeAddress.class, 1);
+        assertNull(homeAddress);
 
-        Member memberEntity = em.find(Member.class, 1);
-        assertNull(memberEntity);
+        Member member = em.find(Member.class, 1);
+        assertNull(member);
     }
 
     @Test
     @Transactional
-    public void Account엔티티저장상태() {
+    public void Account_엔티티저장상태() {
         accountTestData();
 
         Account account = em.find(Account.class, 1);
         assertThat("nklee", is(account.getName()));
         assertThat(2, is(account.getPhones().size()));
+        assertThat(2, is(account.getAddressList().size()));
+    }
+
+    @Test
+    @Transactional
+    public void Account_엔티티삭제() {
+        accountTestData();
+
+        Account account = em.find(Account.class, 1);
+        em.remove(account);
+        em.flush();
+
+        account = em.find(Account.class, 1);
+        assertNull(account);
+
+        AccountPhone accountPhone = em.find(AccountPhone.class, 1);
+        assertNull(accountPhone);
+    }
+
+
+    @Test(expected = AssertionError.class)
+    @Transactional
+    public void AccountPhone_엔티티삭제_실패() {
+        accountTestData();
+
+        AccountPhone accountPhone = em.find(AccountPhone.class, 1);
+        em.remove(accountPhone);
+        em.flush();
+        em.clear();
+
+        accountPhone = em.find(AccountPhone.class, 1);
+        assertNull(accountPhone);
+
+        fail("AccountPhone 엔티티 제거 안 된다. ");
+    }
+
+    @Test
+    @Transactional
+    public void AccountPhone_엔티티삭제_성공() {
+        accountTestData();
+
+        // 삭제 대상
+        AccountAddress accountAddress = em.find(AccountAddress.class, 1);
+
+        // 부모 엔티티에서 삭제 대상의 엔티티를 제거
+        Account account = em.find(Account.class, 1);
+        account.removeAddress(accountAddress); // addressList 필드에 orphanRemoval = true 가 선언되어 있어야 한다.
+
+        // 삭제 SQL DB로 전송
+        em.flush();
+        em.clear();
+
+        accountAddress = em.find(AccountAddress.class, 1);
+        assertNull(accountAddress);
     }
 
 
@@ -141,23 +205,35 @@ public class CascadeTypeAllTest {
  * 테이블 구조
  * ------------------------------------------------------------
  * <p>
- * create table test_cascade_home_address (
- * home_address_id integer not null,
+ * create table test_cascade_account (
+ * account_id integer not null,
+ * account_name varchar(255),
+ * primary key (account_id)
+ * )
+ * <p>
+ * create table test_cascade_account_address (
+ * account_address_id integer not null,
  * address varchar(255),
- * member_id integer,
- * primary key (home_address_id)
+ * account_id integer,
+ * primary key (account_address_id)
  * )
  * <p>
- * create table test_cascade_member (
- * member_id integer not null,
- * name varchar(255),
- * primary key (member_id)
+ * create table test_cascade_account_phone (
+ * account_phone_id integer not null,
+ * account_phone_number varchar(255),
+ * account_id integer,
+ * primary key (account_phone_id)
  * )
  * <p>
- * alter table test_cascade_home_address
- * add constraint FKh0bdfsvahvux2qar39s7ci780
- * foreign key (member_id)
- * references test_cascade_member
+ * alter table test_cascade_account_address
+ * add constraint FKsarc0f3e1004ibkuud7xm9u32
+ * foreign key (account_id)
+ * references test_cascade_account
+ * <p>
+ * alter table test_cascade_account_phone
+ * add constraint FK3o8qx5csp41scgcgiu0a3n17l
+ * foreign key (account_id)
+ * references test_cascade_account
  */
 @Data
 @Entity
@@ -216,7 +292,7 @@ class HomeAddress {
  */
 
 @Data
-@ToString(exclude = "phones")
+@ToString(exclude = {"phones", "addressList"})
 @Entity
 @Table(name = "TEST_CASCADE_ACCOUNT")
 class Account {
@@ -231,9 +307,21 @@ class Account {
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "account", cascade = CascadeType.ALL)
     private List<AccountPhone> phones = new ArrayList<>();
 
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<AccountAddress> addressList = new ArrayList<>();
+
     public void addPhone(AccountPhone accountPhone) {
         this.phones.add(accountPhone);
         accountPhone.setAccount(this); // 해당 코드가 없으면 TEST_CASCADE_ACCOUNT_PHONE 테이블의 ACCOUNT_ID 값이 null로 된다.
+    }
+
+    public void addAddress(AccountAddress accountAddress) {
+        this.addressList.add(accountAddress);
+        accountAddress.setAccount(this);
+    }
+
+    public void removeAddress(AccountAddress accountAddress) {
+        this.addressList.remove(accountAddress);
     }
 }
 
@@ -249,6 +337,23 @@ class AccountPhone {
 
     @Column(name = "ACCOUNT_PHONE_NUMBER")
     private String number;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "ACCOUNT_ID")
+    private Account account;
+}
+
+@Data
+@Entity
+@Table(name = "TEST_CASCADE_ACCOUNT_ADDRESS")
+class AccountAddress {
+
+    @Id
+    @Column(name = "ACCOUNT_ADDRESS_ID")
+    private int id;
+
+    @Column(name = "ADDRESS")
+    private String address;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "ACCOUNT_ID")
